5bd3542e2a31bd4e03cf4fbeb515dcef8d257d67
Tim Skillman <tskillman@silhouettesoftware.com>
2024-08-21 23:30:50 +0100

    Improved SkyBox type

--- SkyBox/main.go
+++ SkyBox/main.go
@@ -8,11 +8,14 @@ func main() {
 	screenWidth := int32(1920)
 	screenHeight := int32(1280)
 
-	rl.InitWindow(screenWidth, screenHeight, "3D Model")
+	rl.InitWindow(screenWidth, screenHeight, "RayLib Go SkyBox")
 	defer rl.CloseWindow()
 
 	skyName := "stormydays_256" // "stormydays_256" "earth" "lowearth_512" "violentdays_256" "miramar_256"
-	skybox := CreateSkyBox("resources/textures/", skyName)
+	skybox := SkyBox{}
+	skybox.CreateSkyBox("resources/textures/", skyName)
+
+	//skybox.CreateSkyBox("resources/textures/", skyName)
 
 	model := rl.LoadModel("resources/models/Asteroid1.obj")
 	texture := rl.LoadTexture("resources/models/msurface.png")
@@ -44,7 +47,7 @@ func main() {
 		rl.DrawModel(model, pos, 0.1, rl.White) // Draw 3d model with texture
 		model.Transform = rl.MatrixRotateXYZ(spin)
 
-		DrawSkyBox(skybox, camera)
+		skybox.DrawSkyBox(camera)
 
 		//rl.DrawGrid(10, 1.0) // Draw a grid
 		rl.EndMode3D() // End 3d mode drawing, returns to orthographic 2d mode
--- SkyBox/skybox.go
+++ SkyBox/skybox.go
@@ -2,31 +2,28 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
-type SkyBox struct {
-	skyPanels [6]rl.Model
-}
+type SkyBox [6]rl.Model
 
-func CreateSkyBox(filePath string, skyName string) SkyBox {
-	skybox := SkyBox{}
+func (sb *SkyBox) CreateSkyBox(filePath string, skyName string) *SkyBox {
 	pi := float32(3.1415926)
 	file := filePath + skyName
 	//var skyPanels [6]rl.Model
-	skybox.skyPanels[0] = SkyBoxFace(file, "_front.png", rl.NewVector3(-pi/2, 0, 0), rl.NewVector3(0, 0, -0.5))
-	skybox.skyPanels[1] = SkyBoxFace(file, "_left.png", rl.NewVector3(pi/2, -pi/2, pi), rl.NewVector3(-0.5, 0, 0))
-	skybox.skyPanels[2] = SkyBoxFace(file, "_back.png", rl.NewVector3(pi/2, 0, -pi), rl.NewVector3(0, 0, 0.5))
-	skybox.skyPanels[3] = SkyBoxFace(file, "_right.png", rl.NewVector3(pi/2, pi/2, -pi), rl.NewVector3(0.5, 0, 0))
-	skybox.skyPanels[4] = SkyBoxFace(file, "_bottom.png", rl.NewVector3(0, 0, 0), rl.NewVector3(0, -0.5, 0))
-	skybox.skyPanels[5] = SkyBoxFace(file, "_top.png", rl.NewVector3(pi, 0, 0), rl.NewVector3(0, 0.5, 0))
-	return skybox
+	sb[0] = skyBoxFace(file, "_front.png", rl.NewVector3(-pi/2, 0, 0), rl.NewVector3(0, 0, -0.5))
+	sb[1] = skyBoxFace(file, "_left.png", rl.NewVector3(pi/2, -pi/2, pi), rl.NewVector3(-0.5, 0, 0))
+	sb[2] = skyBoxFace(file, "_back.png", rl.NewVector3(pi/2, 0, -pi), rl.NewVector3(0, 0, 0.5))
+	sb[3] = skyBoxFace(file, "_right.png", rl.NewVector3(pi/2, pi/2, -pi), rl.NewVector3(0.5, 0, 0))
+	sb[4] = skyBoxFace(file, "_bottom.png", rl.NewVector3(0, 0, 0), rl.NewVector3(0, -0.5, 0))
+	sb[5] = skyBoxFace(file, "_top.png", rl.NewVector3(pi, 0, 0), rl.NewVector3(0, 0.5, 0))
+	return sb
 }
 
-func DrawSkyBox(skybox SkyBox, camera rl.Camera) {
+func (sb *SkyBox) DrawSkyBox(camera rl.Camera) {
 	for i := 0; i < 6; i++ {
-		rl.DrawModel(skybox.skyPanels[i], camera.Position, 1, rl.White)
+		rl.DrawModel(sb[i], camera.Position, 1, rl.White)
 	}
 }
 
-func SkyBoxFace(fileName string, sideName string, rot rl.Vector3, pos rl.Vector3) rl.Model {
+func skyBoxFace(fileName string, sideName string, rot rl.Vector3, pos rl.Vector3) rl.Model {
 	skyBoxSize := float32(500.0)
 	skyPanel := rl.LoadModelFromMesh(rl.GenMeshPlane(skyBoxSize, skyBoxSize, 1, 1))
 	skyPanelTex := rl.LoadTexture(fileName + sideName)
